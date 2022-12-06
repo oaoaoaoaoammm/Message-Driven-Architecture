08657a766b5a780829fa0f143f561601e27726a5
D <kritantablake@gmail.com>
2022-12-06 16:48:21 +0300

    add ikoLite.Notification

---
+++ hw2/Program 3.cs
@@ -0,0 +1,15 @@
+﻿using ikoLite.Notification;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+ConnectionBuilder(args).Build().Run();
+
+static IHostBuilder ConnectionBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices(
+        (hostContext, services) =>
+        {
+            services.AddHostedService<Worker>();
+        });
+
---
+++ hw2/Worker.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using ikoLite.Messaging;
+using Microsoft.Extensions.Hosting;
+
+namespace ikoLite.Notification
+{
+	public class Worker : BackgroundService
+	{
+		private readonly Consumer _consumer;
+
+
+		public Worker()
+		{
+			_consumer = new("BookingNotification", "rattlesnake.rmq.cloudamqp.com");
+		}
+
+
+		protected override async Task ExecuteAsync(CancellationToken token)
+		{
+			_consumer.Recieve((sender, args) =>
+			{
+				var body = args.Body.ToArray();
+				var message = Encoding.UTF8.GetString(body);
+				Console.WriteLine("[x] received {0}", message);
+			});
+		}
+	}
+}
+
