f85e5cbf1dc411851bd56c0cd5bb18ce63b5ed6f
D <kritantablake@gmail.com>
2022-12-11 17:51:05 +0300

    add ikoLite.Kitchen

---
+++ hw3/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ikoLite.Messaging;
+using MassTransit;
+
+namespace ikoLite.Kitchen
+{
+	public class Manager
+	{
+		private readonly IBus _bus;
+
+
+		public Manager(IBus bus)
+		{
+			_bus = bus;
+		}
+
+
+		public void CheckKitchenready(Guid guid, Dish? dish)  //add random events
+        {
+			_bus.Publish<IKitchenReady>(new KitchenReady(guid, true));
+		}
+	}
+}
+
