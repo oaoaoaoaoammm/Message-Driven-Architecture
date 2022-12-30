106324361656292e47be3c91830d184009572df6
D <kritantablake@gmail.com>
2022-12-30 18:30:39 +0300

    add memory

---
+++ hw6/Memory/IMemoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ikoLite.Messaging
+{
+	public interface IMemoryRepository<T> where T : class
+	{
+		public void Add(T entity);
+		public void Update(T entity);
+		public IEnumerable<T> Get();
+	}
+}
+
